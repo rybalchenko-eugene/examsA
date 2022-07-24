7b1665edc81771a35580e9d49d26f5fff71d1759
rybalchenko-eugene <106405926+rybalchenko-eugene@users.noreply.github.com>
2022-07-24 15:52:57 +0300

    Готовая программа

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿string s = "Этот массив содержит элементы с arr ay[0] по array[4]. Элементы массива инициализируются, до значения по умолчанию для типа элемента. Для целых чисел это 0.";
+string[] array = s.Split(" ");
+int count = 0;
+// создание результирующего массива
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        count++;
+    }
+}
+string[] res = new string[count];
+//заполнение нового массива
+int pos = 0;
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j].Length <= 3)
+    {
+        res[pos] = array[j];
+        pos++;
+    }
+}
+// вывод
+for (int i = 0; i < res.Length; i++)
+{
+    Console.Write(res[i] + " ");
+}
+
+
